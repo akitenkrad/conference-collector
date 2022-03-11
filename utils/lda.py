2755d1e7a3d5c168f43b8cfed34700ee1bdd9875
akitenkrad <akitenkrad@gmail.com>
2022-03-11 15:56:21 +0900

    update lda.py

--- utils/lda.py
+++ utils/lda.py
@@ -7,11 +7,16 @@ nltk.download('punkt', quiet=True)
 nltk.download('averaged_perceptron_tagger', quiet=True)
 
 class LDA(object):
-    def __init__(self, texts:str, num_topics=10):
+    def __init__(self, texts:List[str], num_topics=10):
         self.num_topics = num_topics
+        texts = [LDA.tokenize(text) for text in texts]
         self.dictionary = Dictionary(texts)
-        corpus = [self.dictionary.doc2bow(text) for text in texts]
+        corpus = self.get_corpus(texts)
         self.lda = LdaModel(corpus=corpus, num_topics=self.num_topics, id2word=self.dictionary)
+
+    def get_corpus(self, texts:List[str]):
+        texts = [LDA.tokenize(text) for text in texts]
+        return [self.dictionary.doc2bow(text) for text in self.texts]
 
     @classmethod
     def tokenize(cls, text:str) -> List[str]:
